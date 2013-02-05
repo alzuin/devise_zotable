ace630bf9d6358a5d3479c475ad16a8a344278d7
alzuin <alberto@azns.it>
2013-02-05 17:32:33 +0100

    Test ritorno dati

--- lib/devise_zotable/model.rb
+++ lib/devise_zotable/model.rb
@@ -20,10 +20,12 @@ module Devise
       def valid_password?(incoming_password)
         valid = Devise::ZotAdapter.valid_credentials?(self.send(Devise.zot_auth_entity), incoming_password)
         if valid && new_record? # Create this record if valid.
-          token=valid
+          self.token=valid
           create
+          return true
+        else
+          return valid
         end
-        return valid
       end
 
       # Set password to nil
